6ee3ddb2d5f05fc77e8576d6e3916252bc1c4827
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-09-10 17:15:45 +0500

    input search value

--- src/Components/weatherApp/weatherApp.jsx
+++ src/Components/weatherApp/weatherApp.jsx
@@ -12,63 +12,71 @@ import { useEffect } from 'react';
 
 export const WeatherApp = () => {
 
-    const [weather, setWeather] = useState();
+    const [weather, setWeather] = useState('');
+    const [search, setSearch] = useState('');
 
-    async function searchWeather() {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=40ed58a2765c4a602efac457943bedcc&units=metric`);
+    async function searchWeather(defaultValue) {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search || defaultValue}&appid=40ed58a2765c4a602efac457943bedcc&units=metric`);
         const data = await res.json();
-        console.log(data);
+        setWeather(data)
+        console.log(data)
+        // const { feels_like, humidity, pressure, temp} = main;
+        // console.log(name, feels_like, humidity, pressure, temp);
     }
 
     useEffect(()=>{
-        searchWeather()
+        searchWeather("Karachi")
     }, []);
+
+    function inputChangeHandler(val) {
+        setSearch(val.target.value)
+    }
 
     return (
         <div className='container'>
             <div className='searchBar'>
-                <input type={"text"} className='searchInput' placeholder="Search" />
+                <input type={"text"} className='searchInput' placeholder="Search" value={search} onChange={inputChangeHandler} />
                 <button className='searchBtn' onClick={searchWeather}>Search</button>
             </div>
-            <CountryName />
-            <Temperature />
-            <FeelsLike />
-            <ForecastDeatails />
+            <CountryName name = {weather.name}/>
+            <Temperature tempToday={weather.main.temp} />
+            <FeelsLike weatherToday={weather.weather[0].main} feelsLike={weather.main.feels_like}/>
+            <ForecastDeatails windSpeed={weather.wind.speed} humidityToday={weather.main.humidity} visibilityToday={weather.visibility} pressureToday= {weather.main.pressure}/>
         </div>
     )
 }
 
-function CountryName() {
+function CountryName({name}) {
     return (
         <div>
             <h2 className='city'>
-                City Name
+                {name}
             </h2>
         </div>
     )
 };
 
-function Temperature() {
+function Temperature({ tempToday }) {
     return (
         <div>
             <h1 className='temp'>
-                30<span style={{ fontSize: "65px" }}><sup >o</sup>C</span>
+                {Math.round(tempToday)}<span style={{ fontSize: "65px" }}><sup >o</sup>C</span>
             </h1>
         </div>
     )
 };
 
-function FeelsLike() {
+function FeelsLike({ weatherToday, feelsLike}) {
     return (
         <div>
             <p className='feelsLike'>
-                <span>Haze,</span> Feels like: <span>32<sup>o</sup></span>
+                <span>{weatherToday},</span> Feels like: <span>{Math.round(feelsLike)}<sup>o</sup></span>
             </p>
         </div>
     )
 };
 
-function ForecastDeatails() {
+function ForecastDeatails({ windSpeed, humidityToday, visibilityToday, pressureToday }) {
     return (
         <div style={{ width: "100% ", display: "flex", justifyContent: "center", alignItems: "center", padding: "10px 5px 0px 5px" }}>
             <div className='detailContainer'>
@@ -76,10 +84,10 @@ function ForecastDeatails() {
                     Forecast Details
                 </p>
                 <div className='boxes'>
-                    <DetailBox iconName={wind_icon} name={"Wind Speed"} value = {"abc"}/>
-                    <DetailBox iconName={humidity_icon} name={"Humidity"} value = {"abc"} />
-                    <DetailBox name={"Visibility"} value = {"abc"} />
-                    <DetailBox name={"Pressure"} value = {"abc"} />
+                    <DetailBox iconName={wind_icon} name={"Wind Speed"} value = {windSpeed}/>
+                    <DetailBox iconName={humidity_icon} name={"Humidity"} value={humidityToday} />
+                    <DetailBox name={"Visibility"} value={visibilityToday} />
+                    <DetailBox name={"Pressure"} value={pressureToday} />
                 </div>
             </div>
         </div>
