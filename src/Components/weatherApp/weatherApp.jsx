e437c94dbfad25ae9c6842ad095812aa07e70da3
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-09-12 13:22:44 +0500

    bg condition + 404 error handling

--- src/Components/weatherApp/weatherApp.jsx
+++ src/Components/weatherApp/weatherApp.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./weatherAppStyle.css";
-// import clear_icon from '../Assets/clear.png';
-// import cloud_icon from '../Assets/cloud.png';
-// import drizzle_icon from '../Assets/drizzle.png';
+import clear_bg from '../Assets/sunny.jpg';
+import cloud_bg from '../Assets/cloudy.jpg';
+import drizzle_bg from '../Assets/Rain.jpg';
 import humidity_icon from '../Assets/humidity.png';
-// import rain_icon from '../Assets/rain.png';
-// import snow_icon from '../Assets/snow.png';
+import thunder_bg from '../Assets/thunder.jpg';
+import snow_bg from '../Assets/snow.jpg';
+import haze_bg from '../Assets/haze.jpg';
 import wind_icon from '../Assets/wind.png';
+import visiblity_icon from '../Assets/eye-solid.svg';
+import pressure_icon from '../Assets/pressure.png';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
@@ -14,66 +17,114 @@ export const WeatherApp = () => {
 
     const [weather, setWeather] = useState('');
     const [search, setSearch] = useState('');
+    const [weatherName, setWeatherName] = useState('');
 
     async function searchWeather(defaultValue) {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search || defaultValue }&appid=40ed58a2765c4a602efac457943bedcc&units=metric`);
-        const data = await res.json();
-        setWeather(data)
-        // console.log(data)
-        // const { feels_like, humidity, pressure, temp} = main;
-        // console.log(name, feels_like, humidity, pressure, temp);
+        try {
+
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search || defaultValue}&appid=40ed58a2765c4a602efac457943bedcc&units=metric`);
+            const data = await res.json();
+            setWeather(data)
+            setWeatherName(data.weather[0].main)
+
+
+            // console.log(data)
+            // const { feels_like, humidity, pressure, temp} = main;
+            // console.log(name, feels_like, humidity, pressure, temp);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         searchWeather("karachi")
     }, []);
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setTime(new Date())
+    //     }, 1000);
+    // }, []);
 
     function inputChangeHandler(val) {
         setSearch(val.target.value)
     }
 
+    function setBackground() {
+        if (weatherName == 'Haze' || weatherName == 'Mist' || weatherName == 'Smoke' || weatherName == 'Dust' || weatherName == 'Fog' || weatherName == 'Sand' || weatherName == 'Ash' || weatherName == 'Squall' || weatherName == 'Tornado') {
+            return (`url(${haze_bg})`)
+        } else if (weatherName == 'Rain' || weatherName == 'Drizzle') {
+            return (`url(${drizzle_bg})`)
+        } else if (weatherName == 'Clouds') {
+            return (`url(${cloud_bg})`)
+        } else if (weatherName == 'Thunderstorm') {
+            return (`url(${thunder_bg})`)
+        } else if (weatherName == 'Snow') {
+            return (`url(${snow_bg})`)
+        } else if (weatherName == 'Clear') {
+            return (`url(${clear_bg})`)
+        }
+    }
+
+    function invertColor() {
+        if (weatherName == 'Haze' || weatherName == 'Mist' || weatherName == 'Smoke' || weatherName == 'Dust' || weatherName == 'Fog' || weatherName == 'Sand' || weatherName == 'Ash' || weatherName == 'Squall' || weatherName == 'Tornado' || weatherName == 'Clear') {
+            return (`invert(100%)`)
+        }
+    }
+
     return (
         <>
-            {weather !== '' ? (
-                <div className='container'>
-            <div className='searchBar'>
-                <input type={"text"} className='searchInput' placeholder="Search" value={search} onChange={inputChangeHandler} />
-                <button className='searchBtn' onClick={searchWeather}>Search</button>
+            <div className='mainContainer'>
+                {weather !== '' ? (
+                    <div className='container' style={{ backgroundImage: setBackground(), width: "100%",backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+                        <div className='searchBar'>
+                            <input type={"text"} className='searchInput' placeholder="Search" value={search} onChange={inputChangeHandler} style={{ filter: invertColor() }} />
+                            <button className='searchBtn' onClick={searchWeather} style={{ filter: invertColor() }} >Search</button>
+                        </div>
+                        {
+                            weather.cod == "404" ? (
+                                <h1>city not found</h1>
+                            ) : (
+                                <>
+                                    <CountryName name={weather?.name ? weather.name : "No data"} fontColor={invertColor()} />
+                                    <Temperature tempToday={weather?.main?.temp ? weather.main.temp : 0} fontColor={invertColor()} />
+                                    <FeelsLike weatherToday={weather?.weather[0]?.main ? weather?.weather[0]?.main : 0} feelsLike={weather.main.feels_like ? weather.main.feels_like : 0} fontColor={invertColor()} />
+                                    <ForecastDeatails windSpeed={weather.wind.speed ? weather.wind.speed : 0} humidityToday={weather.main.humidity ? weather.main.humidity : 0} visibilityToday={weather.visibility ? weather.visibility : 0} pressureToday={weather.main.pressure ? weather.main.pressure : 0} />
+                                </>
+                            )
+                        }
+
+                    </div>
+                ) : (<h1>loading</h1>)}
             </div>
-            <CountryName name={weather?.name ? weather.name : "No data"}/>
-            <Temperature tempToday={weather?.main?.temp ? weather.main.temp : 0} />
-            <FeelsLike weatherToday={weather?.weather[0]?.main ? weather?.weather[0]?.main : 0} feelsLike={weather.main.feels_like ? weather.main.feels_like : 0}/>
-            <ForecastDeatails windSpeed={weather.wind.speed ? weather.wind.speed : 0} humidityToday={weather.main.humidity ? weather.main.humidity : 0} visibilityToday={weather.visibility ? weather.visibility : 0} pressureToday={weather.main.pressure ? weather.main.pressure : 0}/>
-        </div>
-        ) : (<h1>loading</h1>)}
         </>
     )
 }
 
-function CountryName({name}) {
+function CountryName({ name, fontColor }) {
     return (
         <div>
-            <h2 className='city'>
+            <h2 className='city' style={{ filter: fontColor }}>
                 {name}
             </h2>
         </div>
     )
 };
 
-function Temperature({ tempToday }) {
+function Temperature({ tempToday, fontColor }) {
     return (
         <div>
-            <h1 className='temp'>
+            <h1 className='temp' style={{ filter: fontColor }}>
                 {Math.round(tempToday)}<span style={{ fontSize: "65px" }}><sup >o</sup>C</span>
             </h1>
         </div>
     )
 };
 
-function FeelsLike({ weatherToday, feelsLike}) {
+function FeelsLike({ weatherToday, feelsLike, fontColor }) {
     return (
         <div>
-            <p className='feelsLike'>
+            <p className='feelsLike' style={{ filter: fontColor }}>
                 <span>{weatherToday},</span> Feels like: <span>{Math.round(feelsLike)}<sup>o</sup></span>
             </p>
         </div>
@@ -88,10 +139,10 @@ function ForecastDeatails({ windSpeed, humidityToday, visibilityToday, pressureT
                     Forecast Details
                 </p>
                 <div className='boxes'>
-                    <DetailBox iconName={wind_icon} name={"Wind Speed"} value = {windSpeed}/>
-                    <DetailBox iconName={humidity_icon} name={"Humidity"} value={humidityToday} />
-                    <DetailBox name={"Visibility"} value={visibilityToday} />
-                    <DetailBox name={"Pressure"} value={pressureToday} />
+                    <DetailBox iconName={wind_icon} name={"Wind Speed"} value={windSpeed + " " + "km/h"} />
+                    <DetailBox iconName={humidity_icon} name={"Humidity"} value={humidityToday + "%"} />
+                    <DetailBox iconName={visiblity_icon} name={"Visibility"} value={visibilityToday + " " + "m"} />
+                    <DetailBox iconName={pressure_icon} name={"Pressure"} value={pressureToday + " " + "mBar"} />
                 </div>
             </div>
         </div>
